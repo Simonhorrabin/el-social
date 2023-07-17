f55f10b07cc925f0aa099b25fe4eaa199985531a
simon.horrabin <simonhorrabin@gmail.com>
2023-07-17 18:57:10 -0400

    mongo setup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
 
 //routes
 
@@ -7,7 +9,19 @@ app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
+app.get('/blog', (req, res) => {
+    res.send('Hello Blog, Im Simon')
+})
 
-app.listen(3000, () => {
-    console.log('Node API app is running on port 3000')
+
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://admin:8566520Sh!@socialblog.40tygsv.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, () => {
+        console.log('Node API app is running on port 3000')
+    })
+    
+}).catch((error) =>{
+    console.log(error)
 })
