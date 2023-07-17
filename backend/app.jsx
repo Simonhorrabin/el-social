07fae9ac6a5bd2f6bf7a91dd71cbd8d519e9615b
simon.horrabin <simonhorrabin@gmail.com>
2023-07-17 17:36:40 -0400

    controllers and routes

--- backend/app.jsx
+++ backend/app.jsx
@@ -1,9 +1,15 @@
-import express from 'express'
-
+import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 
-app.use("/", (req, res, next) => {
-    res.send("Hello World");
-});
+mongoose
+  .connect('mongodb+srv://simonhorrabin:xQ2xjl6ibXPS2VFM@cluster0.k08c7w1.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(5000))
+  .then(() => console.log("Connected to database and listening to localhost 5000"))
+  .catch((err) => console.log(err));
 
-app.listen(5000)
+
+
